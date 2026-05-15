cdd53c4c9355134143d32299760dad230d6bcac1
AlgorithmicPV <vidunithap@gmail.com>
2026-05-16 11:19:35 +1200

    Implement font family selection in document editor

--- app/static/js/user/documents/editor-v2.js
+++ app/static/js/user/documents/editor-v2.js
@@ -9,13 +9,21 @@ document.addEventListener('htmx:load', () => {
 
   if (!workspace) { return }
 
+
   const boldBtn = document.getElementById('bold-btn')
   const italicBtn = document.getElementById('italic-btn')
   const underlineBtn = document.getElementById('underline-btn')
   const fontSizeInputBox = document.getElementById('font-size')
+  const selections = document.getElementById('selections')
+  const selectedFont = document.getElementById('selected-font')
 
   let fontSize = 16
   const navKeys = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown']
+  let font
+  let isSelectionHide = true
+
+
+
 
   let editor = new Editor({
     element: workspace,
@@ -38,8 +46,10 @@ document.addEventListener('htmx:load', () => {
 
   })
 
+
   editor.commands.setFontSize(`${fontSize}px`)
   const docPage = document.querySelector('.tiptap')
+
 
   const boldbtnActivated = () => {
     if (editor.isActive('bold')) {
@@ -48,6 +58,7 @@ document.addEventListener('htmx:load', () => {
       boldBtn.classList.remove('active')
     }
   }
+
 
   const italicbtnActivated = () => {
     if (editor.isActive('italic')) {
@@ -66,16 +77,19 @@ document.addEventListener('htmx:load', () => {
     }
   }
 
+
   const updateFontSize = () => {
     if (fontSize == '') return // If user clicks somewhere except the text, there would be no font size, so if it is true, it will return and stop the rest of the function
     fontSizeInputBox.value = parseFloat(fontSize) // Convert the px value to an int
   }
+
 
   const updateFontStyle = () => {
     boldbtnActivated();
     underlineBtnActivated();
     italicbtnActivated();
   }
+
 
   const getElementAtCursor = () => {
     const selection = window.getSelection();
@@ -85,6 +99,18 @@ document.addEventListener('htmx:load', () => {
     }
     return null;
   }
+
+
+  const toggleHideShowSelection = () => {
+    if (isSelectionHide) {
+      selections.classList.remove("hidden")
+      isSelectionHide = false
+    } else {
+      selections.classList.add("hidden")
+      isSelectionHide = true
+    }
+  }
+
 
   document.addEventListener('keydown', (event) => {
     if (event.ctrlKey) {
@@ -99,26 +125,43 @@ document.addEventListener('htmx:load', () => {
   })
 
 
+  selections.addEventListener('click', (event) => {
+    let clickedFontFamily = event.target
+    font = clickedFontFamily.dataset.value;
+    console.log(font)
+    editor.chain().focus().setFontFamily(font).run()
+    selectedFont.querySelector('span').innerText = event.target.innerText
+    toggleHideShowSelection()
+  })
+
+
   boldBtn.addEventListener('click', () => {
     editor.chain().focus().toggleBold().run()
     boldbtnActivated();
   })
+
 
   italicBtn.addEventListener('click', () => {
     editor.chain().focus().toggleItalic().run()
     italicbtnActivated()
   })
 
+
   underlineBtn.addEventListener('click', () => {
     editor.chain().focus().toggleUnderline().run()
     underlineBtnActivated()
   })
+
 
   fontSizeInputBox.addEventListener('keyup', (event) => {
     if (event.key.toLowerCase() === 'enter') {
       editor.commands.setFontSize(`${fontSizeInputBox.value}px`)
     }
   })
+
+
+  selectedFont.addEventListener('click', toggleHideShowSelection)
+
 
   // Update the font size input box value, when the user uses the mouse cursor to select texts on the page
   docPage.addEventListener('mouseup', (event) => {
@@ -127,6 +170,7 @@ document.addEventListener('htmx:load', () => {
     updateFontSize()
     updateFontStyle()
   })
+
 
   docPage.addEventListener('keyup', (event) => {
     let clickedKey = event.key
@@ -137,4 +181,9 @@ document.addEventListener('htmx:load', () => {
       updateFontStyle()
     }
   })
+
+  workspace.addEventListener('click', () => {
+    selections.classList.add('hidden')
+  })
+
 })
--- app/static/js/user/documents/functionality.js
+++ app/static/js/user/documents/functionality.js
@@ -1,8 +1,18 @@
 const selectedFont = document.getElementById('selected-font')
 const selections = document.getElementById('selections')
 
+
 isSelectionHide = true
 
+
 selectedFont.addEventListener('click', () => {
-  console.log(selections)
+  if (isSelectionHide) {
+    selections.style.display = "flex"
+    isSelectionHide = false
+  } else {
+    selections.style.display = "none"
+    isSelectionHide = true
+  }
 })
+
+
